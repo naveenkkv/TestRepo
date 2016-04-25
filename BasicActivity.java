243b1ae9bf742fd46dc2b5fc76c6f38deabbd02d
naveenkumark <naveenkumarki@google.com>
2016-04-25 13:04:51 -0700

    Test commit with changes

--- BasicActivity.java
+++ BasicActivity.java
@@ -5,16 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class BasicActivity extends AppCompatActivity {
+    public static final String TAG = BasicActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_basic);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
